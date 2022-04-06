4a54120001ef1de0140bef6e86dc77eccf433671
GABRIELA MARIA ROCHA BOLAINA <gabibolaina@gmail.com>
2022-04-06 15:36:38 -0300

    Ajustes JS
    Ajustes JS

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -1,3 +1,23 @@
 function calculoJuros() {
+    
+    var tipoJuros = document.getElementById('cTipoJuro').value;
+    var valorInicial = Number(document.getElementById('cValorInicial').value);
+    var tempo = Number.parseInt(document.getElementById('cTempo').value);
+    var rentabilidade = (Number(document.getElementById('cRentabilidade').value))/100;
 
+    /* para juros simples */
+    if(tipoJuros == 'JS'){
+        var total = valorInicial + valorInicial*tempo*rentabilidade;
+    }
+
+    /* fim juros simples */
+    
+    /* para juros compostos */
+    if(tipoJuros == 'JC'){
+    }
+    /* fim juros composto */
+
+    
+    
+    document.getElementById('cTotal').value = total;
 }
